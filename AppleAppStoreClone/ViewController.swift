a0d8f19ef29909e2f56e90f5ba29aefa16096e0d
Ronald Hernandez <rhernandez@solstice-mobile.com>
2016-12-07 13:21:55 -0600

    Initial Commit

---
+++ AppleAppStoreClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppleAppStoreClone
+//
+//  Created by Ronald Hernandez on 12/7/16.
+//  Copyright © 2016 Ronaldoh1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
